a8ee9421e5728e393dc975332e5f23ea25bfb617
Md. Naimul Islam <naimul1818@gmail.com>
2025-10-07 17:38:03 +0600

    lesson-3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ticket;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\TicketFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
+            'password' => Hash::make('password'),
             'email' => 'test@example.com',
         ]);
+
+        Ticket::factory(50)->recycle($users)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/registration', [AuthController::class, 'registration']);
+
+
+Route::get('/tickets', fn() => response()->json(Ticket::all()));
